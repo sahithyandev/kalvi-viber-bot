1635c62b75f4e173d6a273aae19976851d87b759
Sahithyan <sahithyan2701@gmail.com>
2021-09-02 21:13:29 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+console.log("started");
+
+
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const ngrok = require("./getPublicUrls");
+const winston = require("winston");
+
 const ViberBot = require("viber-bot").Bot;
 const BotEvents = require("viber-bot").Events;
 
@@ -8,24 +14,51 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
+const IS_PRODUCTION = process.env.IS_PRODUCTION;
+
 const VIBER_BOT_ROUTE = "/viber";
 
 const bot = new ViberBot({
-	authToken: API_KEY,
-	name: "BoyTest",
-	avatar: path.join(process.env.VERCEL_URL, "/public/profile.jpg")
+  logger: IS_PRODUCTION
+    ? undefined
+    : new winston.createLogger({
+        level: IS_PRODUCTION ? "info" : "debug",
+      }),
+  authToken: API_KEY,
+  name: "BoyTest",
+  avatar: "https://static.botsrv2.com/website/img/quriobot_favicon.1727b193.png",
 });
-
-console.log(process.env);
 
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-	response.send(message);
+  console.log("message-received", message);
+  response.send(message);
 });
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 app.use(VIBER_BOT_ROUTE, bot.middleware());
 
 app.listen(PORT, () => {
-	const webhookUrl = path.join(process.env.VERCEL_URL, VIBER_BOT_ROUTE);
-	bot.setWebhook(webhookUrl);
+  (async () => {
+    if (IS_PRODUCTION) {
+      return `https://${process.env.VERCEL_URL}${VIBER_BOT_ROUTE}`;
+    } else {
+      const ngrokUrl = await ngrok.getPublicUrl();
+      return `${ngrokUrl}${VIBER_BOT_ROUTE}`;
+    }
+  })()
+    .then((webhookUrl) => {
+      console.log("adding webhook", webhookUrl);
+      bot
+        .setWebhook(webhookUrl)
+        .then(() => {
+          console.log("webhook added");
+        })
+        .catch((err) => {
+          console.warn("error while adding webhook", err);
+        });
+    })
+    .catch((error) => {
+      console.log("Can not connect to ngrok server. Is it running?");
+      console.error(error);
+    });
 });
