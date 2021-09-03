a93d8f3fe81bc1649a582ad121a8ab0923bb3ef5
Sahithyan <sahithyan2701@gmail.com>
2021-09-03 12:28:10 +0530

    file sharing working

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const express = require("express");
 const ngrok = require("./getPublicUrls");
 const winston = require("winston");
 
-const ViberBot = require("viber-bot").Bot;
-const BotEvents = require("viber-bot").Events;
+const viberBotPackage = require("viber-bot");
+const ViberBot = viberBotPackage.Bot;
+const BotEvents = viberBotPackage.Events;
 
 const app = express();
+const path = require("path");
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
@@ -16,6 +19,7 @@ const IS_PRODUCTION = process.env.IS_PRODUCTION === "true";
 
 const VIBER_BOT_ROUTE = "/viber";
 
+let hostUrl = "";
 const bot = new ViberBot({
   logger: IS_PRODUCTION
     ? undefined
@@ -23,33 +27,64 @@ const bot = new ViberBot({
         level: IS_PRODUCTION ? "info" : "debug",
       }),
   authToken: API_KEY,
-  name: "BoyTest",
-  avatar: "https://static.botsrv2.com/website/img/quriobot_favicon.1727b193.png",
+  name: process.env.BOT_NAME,
+  avatar:
+    "https://static.botsrv2.com/website/img/quriobot_favicon.1727b193.png",
 });
 
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+async function getPdfSize(filename) {
+  const fs = require("fs/promises");
+  let file;
+  try {
+    file = await fs.open(filename, "r");
+    const stat = await file.stat();
+    return stat.size;
+  } catch {
+    await file.close();
+  }
+}
+
+bot.on(BotEvents.MESSAGE_RECEIVED, async (message, response) => {
   console.log("message-received", message);
+
+  if (message.text === "pdf") {
+    const filename = "quantum.pdf";
+    const fileLocation = "public/quantum.pdf";
+    const fileSize = await getPdfSize(fileLocation);
+
+    response.send(
+      new viberBotPackage.Message.File(
+        `${hostUrl}/${fileLocation}`,
+        fileSize,
+        filename
+      )
+    );
+    return;
+  }
+
   response.send(message);
 });
 
 app.use(VIBER_BOT_ROUTE, bot.middleware());
 
 app.get("/", (req, res) => {
-	console.log("sss");
-	res.send("done");
+  res.send("running");
 });
 
 app.listen(PORT, () => {
+  console.log("listening");
   (async () => {
-		console.log("is prod", IS_PRODUCTION);
+    console.log("is prod", IS_PRODUCTION);
     if (IS_PRODUCTION) {
-      return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com${VIBER_BOT_ROUTE}`;
+      return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
     } else {
       const ngrokUrl = await ngrok.getPublicUrl();
-      return `${ngrokUrl}${VIBER_BOT_ROUTE}`;
+      return ngrokUrl;
     }
   })()
-    .then((webhookUrl) => {
+    .then((_hostUrl) => {
+      hostUrl = _hostUrl;
+      const webhookUrl = `${_hostUrl}${VIBER_BOT_ROUTE}`;
       console.log("adding webhook", webhookUrl);
       bot
         .setWebhook(webhookUrl)
