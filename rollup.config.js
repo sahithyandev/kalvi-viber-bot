6110942148401aa4db644caa8d7ac8a2feb95fa2
Sahithyan <sahithyan2701@gmail.com>
2021-09-07 16:12:05 +0530

    typescript migration 2

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
 import typescript from "@rollup/plugin-typescript";
 
-export default {
+/**
+ * @type {import("rollup").RollupOptions}
+ */
+const options = {
 	input: "src/index.ts",
 	output: {
 		file: "dist/index.js",
 		format: "cjs",
 	},
 	plugins: [typescript()],
+	watch: {
+		clearScreen: false,
+		exclude: ["node_modules/**/*"],
+		include: "src/**/*",
+		buildDelay: 6000,
+		chokidar: { usePolling: true },
+	},
 	external: ["viber-bot"],
 };
+
+export default options;
