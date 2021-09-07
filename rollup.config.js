a96ccc612e5c8f95ecb6a2a6f0b395a5c387a084
Sahithyan <sahithyan2701@gmail.com>
2021-09-07 10:24:56 +0530

    typescript basic setup

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+	input: "src/index.js",
+	output: {
+		file: "dist/index.js",
+		format: "cjs",
+	},
+	plugins: [typescript()],
+	external: ["viber-bot"],
+};
